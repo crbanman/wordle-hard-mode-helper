e0fc12bc632fd495fc09c2ca2b0a10ed0066dcde
Cody Banman <crbanman@gmail.com>
2023-05-19 08:25:08 -0700

    fix: wordle's new welcome page breaks the extension

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,19 @@
-const game = document.querySelector("#wordle-app-game");
-const keyboard = game.querySelector('[class^="Keyboard-module_keyboard"]');
+let game = document.querySelector("#wordle-app-game");
+let keyboard = game?.querySelector('[class^="Keyboard-module_keyboard"]');
 const backspaceKey = "←";
+
+const gameWatcher = new MutationObserver((mutations, observer) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type !== "childList") return;
+
+    if (document.querySelector("#wordle-app-game") !== null) {
+      game = document.querySelector("#wordle-app-game");
+      keyboard = game.querySelector('[class^="Keyboard-module_keyboard"]');
+      gameObserver.observe(game, { childList: true, subtree: true });
+      observer.disconnect();
+    }
+  });
+});
 
 const gameObserver = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
@@ -20,4 +33,11 @@ const gameObserver = new MutationObserver((mutations) => {
   });
 });
 
-gameObserver.observe(game, { childList: true, subtree: true });
+// NYTimes added a welcome screen and I'm not sure if it's always there.
+// So if we don't find the game, we'll watch changes to the body until
+// it's there.
+if (game === null) {
+  gameWatcher.observe(document.body, { childList: true, subtree: true });
+} else {
+  gameObserver.observe(game, { childList: true, subtree: true });
+}
